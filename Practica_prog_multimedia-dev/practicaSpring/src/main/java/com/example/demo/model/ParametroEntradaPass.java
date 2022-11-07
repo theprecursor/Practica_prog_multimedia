a9e66fd36dbdee501dda1d0320ad7bc9a1075204
SpaceAlbaricoque <105358734+SpaceAlabaricoque@users.noreply.github.com>
2022-11-07 06:21:38 +0100

    Add files via upload
    No tiene lo del email. Tiene para poder hacer un registro y añadir datos y para poder visualizar los datos de los usuarios. Lo de la contraseña esta a medio hacer, digo medio por que son las 6:30 de la mañan y quiero dormir algo

---
+++ Practica_prog_multimedia-dev/practicaSpring/src/main/java/com/example/demo/model/ParametroEntradaPass.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+public class ParametroEntradaPass {
+	String pass;
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	
+}
